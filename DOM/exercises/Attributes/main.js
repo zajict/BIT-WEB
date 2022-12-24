b426b482ed64c58924d9bc1b9d6100bfb35fa2e3
Tijana Zajic <tijanazajic1204@gmail.com>
2022-12-24 17:28:54 +0100

    change object exercises

--- DOM/exercises/Attributes/main.js
+++ DOM/exercises/Attributes/main.js
@@ -1,17 +1,24 @@
  /* Create a form that contains multiple input elements with labels.Some of those elements should be required.Create a function that validates the form.The function checks each input element that belongs to the form.If the element has the required attribute and no data,
 it should get red borders. */
-
-function validateForm() { 
+function validateForm() {
     var inputs = document.querySelectorAll("#form input");
 
-    inputs.forEach(function (input) { 
+    var valid = true;
+
+    inputs.forEach(function (input) {
         if (input.hasAttribute("required") && input.value === "") { 
-            input.style.border = "2px solid yellow";
-        } else { 
+            input.style.border = "2px solid red";
+
+            valid = false;
+        } else {
             input.style.border = "";
         }
     });
+
+    return valid;
 }
+validateForm()
+
 
 
 
