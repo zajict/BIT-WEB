436d165fb6a5a3ecdb92e840a23ff51e6a85d4cd
Tijana Zajic <tijanazajic1204@gmail.com>
2023-02-02 19:36:13 +0100

    Ajax exercises

--- AJAX/JSON/script.js
+++ AJAX/JSON/script.js
@@ -1,22 +1,21 @@
-function getDogImage() { // Send an AJAX request to the Dog API
+var button = document.querySelector('#button');
+
+function getDogImage() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
-    xhr.send();
 
-    // Handle the response
     xhr.onload = function () {
-        if (xhr.status === 200) { // Extract the image URL from the response
+        if(xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
-            var imageUrl = response.message;
+            var imageURL = response.message;
 
-            // Create an image element and set its src to the image URL
             var image = document.createElement("img");
-            image.src = imageUrl;
-
-            // Add the image to the HTML page
+            image.src = imageURL;
             document.getElementById("dog-images").appendChild(image);
         } else {
-            console.error("An error occurred: " + xhr.status);
+            console.log("Error" + xhr.status);
         }
     };
+    xhr.send();
 }
+button.addEventListener("click", getDogImage);
--- AJAX/XML/script.js
+++ AJAX/XML/script.js
@@ -1,13 +1,15 @@
+var button = document.querySelector('#button');
+
 
 function getCountryByIP() {
     var xhr = new XMLHttpRequest();
     var ipAddress = document.getElementById("input").value;
 
-    xhr.open('GET', 'http://www.geoplugin.net/xml.gp?ip=xx.xx.xx.xx', true);
+    ipAddress.value = xhr.open('GET', 'http://www.geoplugin.net/xml.gp?ip=xx.xx.xx.xx', true);
 
 
     xhr.onload = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.status === 200) {
             var xmlDoc = xhr.responseXML;
             var countryName = xmlDoc.getElementsByTagName("geoplugin_countryName")[0].childNodes[0].nodeValue;
             document.getElementById("result").innerHTML = countryName;
@@ -15,3 +17,5 @@ function getCountryByIP() {
     };
     xhr.send();
 }
+button.addEventListener("click", getCountryByIP);
+
