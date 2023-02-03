f040f67dc8cf6a880cc9dda1bc907776edb457d9
Tijana Zajic <tijanazajic1204@gmail.com>
2023-02-03 13:59:41 +0100

    git search

--- AJAX/GitHubSearcher/script.js
+++ AJAX/GitHubSearcher/script.js
@@ -1,19 +1,19 @@
-$("#search-button").click(function () {
-    var searchTerm = $("#search-input").val();
-    var url = "https://api.github.com/search/users?q=" + searchTerm;
-    $.ajax({
-        url: url,
-        type: "GET",
-        success: function (response) {
-            $("#search-results").empty();
-            response.items.forEach(function (user) {
-                var html = "<div class='search-result'>";
-                html += "<img src='" + user.avatar_url + "' alt='" + user.login + "' />";
-                html += "<span>" + user.login + "</span>";
-                html += "</div>";
-                $("#search-results").append(html);
-            });
-        }
-    });
-});
+// $("#search-button").click(function () {
+//     var searchTerm = $("#search-input").val();
+//     var url = "https://api.github.com/search/users?q=" + searchTerm;
+//     $.ajax({
+//         url: url,
+//         type: "GET",
+//         success: function (response) {
+//             $("#search-results").empty();
+//             response.items.forEach(function (user) {
+//                 var html = "<div class='search-result'>";
+//                 html += "<img src='" + user.avatar_url + "' alt='" + user.login + "' />";
+//                 html += "<span>" + user.login + "</span>";
+//                 html += "</div>";
+//                 $("#search-results").append(html);
+//             });
+//         }
+//     });
+// });
 
